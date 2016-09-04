c0b378703bec944a441220894a56581e0567b1ce
Jean-François YUEN <jfyuen@gmail.com>
2016-09-04 14:07:33 +0200

    much simpler version

--- 1_string_arrays/rotate_words_in_string.py
+++ 1_string_arrays/rotate_words_in_string.py
@@ -1,33 +1,8 @@
-def rotate_words(string):
-    j = 0
-    for i, s in enumerate(string):
-        if s == ' ':
-            string = reverse(string, j, i-1)
-            j = i + 1
-
-    string = reverse(string, j, len(string)-1)
-    string = reverse(string, 0, len(string)-1)
-
-    return string
-
-def reverse(string, left, right):
-    string = list(string)
-    # Python strings are immutable so they do not support assignment.
-    # So we cast the string into a list first, and we join the list into a str
-    # at the end before returning.
-    if len(string) == 0 or len(string) == 1:
-        return string
-    while left < right:
-        temp = string[left]
-        string[left] = string[right]
-        string[right] = temp
-        left += 1
-        right -= 1
-    string = "".join(string)
-    return string
+def rotate_words(str):
+    words = str.split(' ')
+    return ' '.join(reversed(words))
 
 if __name__ == "__main__":
     string = 'the sky is blue'
     ginrts = rotate_words(string)
     print(ginrts)
-
