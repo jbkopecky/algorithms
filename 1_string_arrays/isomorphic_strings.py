122930d3c7ee83cc8c19be2ab8e024a205c913fc
Jean-François YUEN <jfyuen@gmail.com>
2016-09-04 14:25:30 +0200

    more idiomatic python

--- 1_string_arrays/isomorphic_strings.py
+++ 1_string_arrays/isomorphic_strings.py
@@ -1,18 +1,18 @@
 def is_isomorphic(s1, s2):
     if len(s1) == 0 or len(s2) == 0:
         return False
-    if not len(s1) == len(s2):
+    if len(s1) != len(s2):
         return False
     map = {}
     for i in range(len(s1)):
         c1 = s1[i]
         c2 = s2[i]
-        if c1 in map.keys():
-            if not c2 == map[c1]:
+        if c1 in map:
+            if c2 != map[c1]:
                 return False
+        elif c2 in map.values():
+            return False
         else:
-            if c2 in map.values():
-                return False
             map[c1] = c2
     return True
 
