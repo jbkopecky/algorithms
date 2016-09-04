fc65aa28814b262cd843d3ab4096c1e55b75eb25
Jean-François YUEN <jfyuen@gmail.com>
2016-09-04 14:01:36 +0200

    slight changes

--- 1_string_arrays/word_ladder.py
+++ 1_string_arrays/word_ladder.py
@@ -1,7 +1,9 @@
 import urllib2
+import heapq
 
 ALPH = 'abcdefghijklmnopqrstuvwxyz'
 DICT_URL = 'https://svnweb.freebsd.org/csrg/share/dict/words?revision=61569&view=co&pathrev=61569'
+
 
 def get_words(length):
     data = urllib2.urlopen(DICT_URL)
@@ -9,42 +11,38 @@ def get_words(length):
     return words
 
 
-class WordNode(object):
-    def __init__(self, word, numstep):
-        self.numstep = numstep
-        self.word = word
-
-
-def word_lader(start_word, end_word, word_set):
-    queue = []
-    queue.append(WordNode(start_word,1))
+def word_ladder(start_word, end_word, word_set):
+    queue = [(1, start_word)]
     word_set.add(end_word)
 
     while len(queue) > 0:
-        top = queue.pop(0)
-        word = top.word
+        numstep, word = queue.pop(0)
 
         if word == end_word:
-            return top.numstep
+            return numstep
 
         arr = list(word)
 
         for i in range(len(arr)):
             for c in ALPH:
+                if arr[i] == c:
+                    continue
                 temp = arr[i]
-                if not arr[i] == c:
-                    arr[i] = c
+                arr[i] = c
                 new_word = ''.join(arr)
+                arr[i] = temp
                 if new_word in word_set:
                     word_set.remove(new_word)
-                    queue.append(WordNode(new_word, top.numstep + 1))
-                arr[i] = temp
+                    queue.append((numstep + 1, new_word))
+    return 0
 
 
 if __name__ == "__main__":
     from_word = 'hit'
     to_word = 'cog'
-    words_set = set(get_words(len(from_word)))
+    
     short_set = set(["hot","dot","dog","lot","log"]) 
-    print(word_lader(from_word, to_word, short_set))
-    print(word_lader(from_word, to_word, words_set))
+    print(word_ladder(from_word, to_word, short_set))
+    
+    words_set = set(get_words(len(from_word)))
+    print(word_ladder(from_word, to_word, words_set))
